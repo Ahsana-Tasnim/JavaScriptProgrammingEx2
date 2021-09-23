65716938d429ee267811adcdf7741cb950ad7da6
Ahsana-Tasnim <ahsana.tasnim@gmail.com>
2021-09-23 09:26:27 -0300

    Created app.js

---
+++ app.js
@@ -0,0 +1,69 @@
+/*
+JavaSciprt Programming Exercise 2
+Strings and atring functions
+Ahsana Tasnim
+*/
+
+let l = "~~~~~~~~~~~~~~~~";
+
+let firstName = 'Ahsana';
+let lastName = "Tasnim";
+let age = 21;
+
+console.log(l);
+//Concatenation
+let messageConcatenation = 'My name is ' + firstName + ' ' + lastName + ' and my age is ' + age;
+console.log(messageConcatenation);
+console.log(l);
+
+//String Template/Template Literals
+let messageStringLiteral = `My name is ${firstName} ${lastName} and my age is ${age}`;
+console.log(messageStringLiteral);
+console.log(l);
+
+//Escape & New Line
+let a = "Add a new line\nThis is a new line";
+console.log(a);
+console.log(l);
+
+let b = "I quoted the person as saying \"I love JacaScript\".";
+console.log(b);
+console.log(l);
+
+let c = 'It\'s right over there.';
+console.log(c);
+console.log(l);
+
+let d = 'I quoted the person as saying "I love JavaScript".';
+console.log(d);
+console.log(l);
+
+let e = "It's right over there.";
+console.log(e);
+console.log(l);
+
+console.log("String Functions");
+let f = "abcdefg";
+
+// Length of f
+console.log(`Length of f - ${f.length}`);
+
+//toUpper
+f = f.toUpperCase();
+console.log(f);
+
+//toLower
+f = f.toLowerCase();
+console.log(f);
+
+//substr
+let g = f.substr(2, 4);
+console.log(g);
+
+//substring
+let h = f.substring(3);
+console.log(h);
+
+//indexOf
+let index = f.indexOf("c");
+console.log(index);
